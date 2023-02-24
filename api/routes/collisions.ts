4c47bb1a851ff189441cffd750c4b88edf2f891e
Tessa Kelly <6866749+tessakelly@users.noreply.github.com>
2023-02-23 19:10:41 -0800

    Add collision charges breakdown API

--- api/routes/collisions.ts
+++ api/routes/collisions.ts
@@ -10,9 +10,41 @@ router.get('/', makeHandlerAwareOfAsyncErrors(async (req: Request, res: Response
         raw: true,
         group: ['mon'],
         order: ['mon'],
-        attributes: [[Sequelize.fn('date_trunc', 'month', Sequelize.col('date_time')), 'mon'], [Sequelize.fn('sum', Sequelize.col('injured')), 'mon_injured'], [Sequelize.fn('sum', Sequelize.col('killed')), 'mon_killed'], [Sequelize.fn('count', Sequelize.col('date_time')), 'total']]
+        attributes: [
+            [Sequelize.fn('date_trunc', 'month', Sequelize.col('date_time')), 'mon'],
+            [Sequelize.fn('sum', Sequelize.col('injured')), 'mon_injured'],
+            [Sequelize.fn('sum', Sequelize.col('killed')), 'mon_killed'],
+            [Sequelize.fn('count', Sequelize.col('date_time')), 'total']]
     });
     res.send(result);
+}));
+
+router.get('/charges', makeHandlerAwareOfAsyncErrors(async (req: Request, res: Response) => {
+    const result: any = await Collision.findAll({
+        raw: true,
+        group: ['charge_desc'],
+        order: [['num', 'desc']],
+        attributes: [
+            'charge_desc',
+            [Sequelize.fn('count', Sequelize.col('charge_desc')), 'num']
+        ]
+    });
+    const NUM_CHARGES_TO_RETURN = 9;
+    let simplifiedResult = result.slice(0, NUM_CHARGES_TO_RETURN).map((x: any) => ({
+        charge_desc: x.charge_desc[0] + x.charge_desc.substring(1).toLowerCase(),
+        count: parseInt(x.num)
+    }));
+
+    const otherCount = result.slice(NUM_CHARGES_TO_RETURN).reduce(
+        (accumulator: number, currentValue: any) => accumulator + parseInt(currentValue.num),
+        0
+    );
+    simplifiedResult.push({
+        charge_desc: 'Other',
+        count: otherCount
+    });
+
+    res.send(simplifiedResult);
 }));
 
 export default router;
